2822fffd1ba11ab94ae275fbdd2e928d3e3aaf39
Boyeon <dlqhdus628@naver.com>
2022-05-02 14:07:08 +0900

    RegisterToken.sol, SaleToken.sol 작성

---
+++ contracts/RegisterToken.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+import "SaleToken.sol";
+
+contract RegisterToken is ERC721URIStorage, ERC721Enumerable {
+    uint256 public tokenCounter;
+    constructor() ERC721("ImageNFT", "INFT") {
+        tokenCounter = 0;
+    }
+
+    SaleToken public saleToken;
+
+    struct TokenData {
+        uint256 tokenId;
+        string tokenUri;
+        uint256 tokenPrice;
+    }
+
+    /// @dev : _tokenURI(metadata-json-hash)로 nft를 등록하는 함수
+    function registerNft(string memory _tokenURI) public {
+        uint256 newItemId = tokenCounter;
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, _tokenURI);
+        tokenCounter = tokenCounter + 1;
+    }
+
+    /// @dev : _tokenOwner가 가진 모든 nft를 반환하는 함수
+    function getToken(address _tokenOwner) view public returns (TokenData[] memory) {
+        uint256 balanceLength = balanceOf(_tokenOwner);
+        require(balanceLength != 0, "Owner did not have token");
+
+        TokenData[] memory tokenData = new TokenData[](balanceLength);
+
+        for (uint256 i=0; i<balanceLength; i++) {
+            uint256 tokenId = tokenOfOwnerByIndex(_tokenOwner, i);
+            string memory tokenUri = tokenURI(tokenId);
+            uint256 tokenPrice = saleToken.getTokenPrice(tokenId);
+
+            tokenData[i] = TokenData(tokenId, tokenUri, tokenPrice);
+        }
+
+        return tokenData;
+    }
+
+    /// @dev : saleToken 사용할 수 있게 하는 함수
+    function setSaleToken(address _saleToken) public {
+        saleToken = SaleToken(_saleToken);
+    }
+
+    /// override 방지
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
---
+++ contracts/SaleToken.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "RegisterToken.sol";
+
+contract SaleToken {
+    RegisterToken public registerToken;
+
+    constructor (address _registerTokenAddress) {
+        registerToken = RegisterToken(_registerTokenAddress);
+    }
+
+    mapping(uint256 => uint256) public tokenPrices;
+
+    uint256[] public onSaleTokenArray;
+
+    /// @dev : 토큰 가격을 설정하는 함수
+    function setForSaleToken(uint256 _tokenId, uint256 _price) public {
+        address tokenOwner = registerToken.ownerOf(_tokenId);
+
+        require(tokenOwner == msg.sender, "Caller is not token owner");
+        require(_price > 0, "Price is zero or lower");
+        require(tokenPrices[_tokenId] == 0, "This token is already on sale");
+        require(registerToken.isApprovedForAll(tokenOwner, address(this)), "Token owner did not approve token");
+
+        tokenPrices[_tokenId] = _price;
+
+        onSaleTokenArray.push(_tokenId);
+    }
+
+    /// @dev : 토큰을 구매하는 함수
+    function purchaseToken(uint256 _tokenId) public payable {
+        uint256 price = tokenPrices[_tokenId];
+        address tokenOwner = registerToken.ownerOf(_tokenId);
+
+        require(price > 0, "Animal token not sale");
+        require(price <= msg.value, "Caller sent lower than price");
+        require(tokenOwner != msg.sender, "Caller is token owner");
+
+        payable(tokenOwner).transfer(msg.value);
+        registerToken.safeTransferFrom(tokenOwner, msg.sender, _tokenId);
+
+        tokenPrices[_tokenId] = 0;
+
+        for(uint256 i=0; i<onSaleTokenArray.length; i++) {
+            if(tokenPrices[onSaleTokenArray[i]] == 0) {
+                onSaleTokenArray[i] = onSaleTokenArray[onSaleTokenArray.length - 1];
+                onSaleTokenArray.pop();
+            }
+        }
+    }
+
+    /// @dev : onSaleTokenArray 길이 반환하는 함수
+    function getOnSaleTokenArrayLength() view public returns(uint256) {
+        return onSaleTokenArray.length;
+    }
+
+    /// @dev : 토큰 가격 반환하는 함수
+    function getTokenPrice(uint256 _tokenId) view public returns(uint256) {
+        return tokenPrices[_tokenId];
+    }
+}
