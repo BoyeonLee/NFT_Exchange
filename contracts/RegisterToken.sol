c778b0f3e8e2435983d74f75b89d5f4b57b67f4b
Boyeon <dlqhdus628@naver.com>
2022-05-04 07:26:26 +0900

    import 주소 수정

--- contracts/RegisterToken.sol
+++ contracts/RegisterToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-import "SaleToken.sol";
+import "./SaleToken.sol";
 
 contract RegisterToken is ERC721URIStorage, ERC721Enumerable {
     uint256 public tokenCounter;
