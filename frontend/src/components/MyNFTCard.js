9698d38f6f572a0c94183e3a88df0b1d24785e31
Boyeon <dlqhdus628@naver.com>
2022-05-09 13:39:17 +0900

    edit button 추가

--- frontend/src/components/MyNFTCard.js
+++ frontend/src/components/MyNFTCard.js
@@ -6,6 +6,8 @@ import { saleTokenContract, web3 } from "../contracts/contracts_info";
 const MyNFTCard = ({ tokenId, tokenUri, tokenPrice, saleStatus, account }) => {
   const [sellPrice, setSellPrice] = useState("");
   const [myPrice, setMyPrice] = useState(tokenPrice);
+  const [isEditable, setIsEditable] = useState(false);
+  const [editPrice, setEditPrice] = useState(web3.utils.fromWei(tokenPrice));
 
   const onChangeSellPrice = (e) => {
     setSellPrice(e.target.value);
@@ -19,19 +21,37 @@ const MyNFTCard = ({ tokenId, tokenUri, tokenPrice, saleStatus, account }) => {
         .setForSaleToken(tokenId, web3.utils.toWei(sellPrice, "ether"))
         .send({ from: account });
 
-      console.log(`setForSaleToken response: ${response}`);
       if (response.status) {
         setMyPrice(web3.utils.toWei(sellPrice, "ether"));
+        setEditPrice(sellPrice);
       }
     } catch (error) {
       console.error(error);
     }
   };
+
+  const onClickEditPrice = async () => {
+    try {
+      if (!account || !saleStatus) return;
+
+      const response = await saleTokenContract.methods
+        .editForSaleToken(tokenId, web3.utils.toWei(editPrice, "ether"))
+        .send({ from: account });
+
+      if (response.status) {
+        setMyPrice(web3.utils.toWei(editPrice, "ether"));
+        setIsEditable(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Box textAlign="center">
       <NFTCard tokenUri={tokenUri} />
       <Box mt={2}>
-        {myPrice === "0" ? (
+        {myPrice === "0" && isEditable === false ? (
           <>
             <InputGroup>
               <Input type="number" value={sellPrice} onChange={onChangeSellPrice}></Input>
@@ -42,9 +62,41 @@ const MyNFTCard = ({ tokenId, tokenUri, tokenPrice, saleStatus, account }) => {
             </Button>
           </>
         ) : (
-          <Text d="inline-block" fontSize="xl" fontWeight="bold">
-            {web3.utils.fromWei(myPrice)} ETH
-          </Text>
+          <Box>
+            {isEditable ? (
+              <>
+                <InputGroup>
+                  <Input
+                    type="number"
+                    value={editPrice}
+                    onChange={(e) => {
+                      setEditPrice(e.target.value);
+                    }}
+                  ></Input>
+                  <InputRightAddon children="ETH" />
+                </InputGroup>
+                <Button size="sm" colorScheme="blue" mt={2} onClick={onClickEditPrice}>
+                  Finish
+                </Button>
+              </>
+            ) : (
+              <>
+                <Text fontSize="xl" fontWeight="bold">
+                  {web3.utils.fromWei(myPrice)} ETH
+                </Text>
+                <Button
+                  size="sm"
+                  colorScheme="pink"
+                  mt={2}
+                  onClick={() => {
+                    setIsEditable(true);
+                  }}
+                >
+                  Edit
+                </Button>
+              </>
+            )}
+          </Box>
         )}
       </Box>
     </Box>
